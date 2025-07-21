952d6f2093496fe8431d1f4e359e616c925e4e77
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-21 12:39:38 +0200

    Create settings.php

---
+++ config/settings.php
@@ -0,0 +1,10 @@
+<?php
+// config/settings.php
+define('SMTP_HOST',   'smtp.seudominio.com');
+define('SMTP_USER',   'no-reply@seudominio.com');
+define('SMTP_PASS',   'smtp_senha');
+define('INSTITUICAO_EMAIL', 'admin@instituicao.co.mz');
+
+// Paths
+define('BASE_URL',    'https://seudominio.com/');
+define('PDF_DIR',     __DIR__ . '/../faturas/');
