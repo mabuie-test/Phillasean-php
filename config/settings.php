32652e8d17c34862f5fab55c228fe5a277000cec
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-21 12:59:44 +0200

    Update settings.php

--- config/settings.php
+++ config/settings.php
@@ -1,10 +1,19 @@
 <?php
-// config/settings.php
-define('SMTP_HOST',   'smtp.seudominio.com');
-define('SMTP_USER',   'no-reply@seudominio.com');
-define('SMTP_PASS',   'smtp_senha');
-define('INSTITUICAO_EMAIL', 'admin@instituicao.co.mz');
+use Dotenv\Dotenv;
 
-// Paths
-define('BASE_URL',    'https://seudominio.com/');
-define('PDF_DIR',     __DIR__ . '/../faturas/');
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+define('DB_HOST',   $_ENV['DB_HOST']);
+define('DB_NAME',   $_ENV['DB_NAME']);
+define('DB_USER',   $_ENV['DB_USER']);
+define('DB_PASS',   $_ENV['DB_PASS']);
+
+define('SMTP_HOST', $_ENV['SMTP_HOST']);
+define('SMTP_USER', $_ENV['SMTP_USER']);
+define('SMTP_PASS', $_ENV['SMTP_PASS']);
+define('INSTITUICAO_EMAIL', $_ENV['INST_EMAIL']);
+
+define('PDF_DIR', __DIR__ . '/../faturas/');
+define('BASE_URL', 'https://seudominio.com/');
